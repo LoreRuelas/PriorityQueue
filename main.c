04989e7331ae117da2c9e2e853345368771eee8b
LoreRuelas <103057085+LoreRuelas@users.noreply.github.com>
2022-11-13 22:24:33 -0600

    Update main.c

--- main.c
+++ main.c
@@ -35,15 +35,23 @@
 
 int main() {
 
-    // Logramos implementar la parte no comentada
+// Logramos implementar la parte no comentada
     PriorityQueue *pq = p_queue_new();
+
+    int result = p_queue_empty(pq);
+    printf("Esta vacio? (0 = No, 1 = SI) %d\n", result);
 
     p_queue_enqueue(pq, "Foo", 12);
     p_queue_enqueue(pq, "Bar", 11);
     p_queue_enqueue(pq, "Hola", 5);
     p_queue_enqueue(pq, "Mundo!", 10);
     p_queue_enqueue(pq, "ESTO ES URGENTE!", 0);
+
+    result = p_queue_empty(pq);
+    printf("Esta vacio? (0 = No, 1 = SI) %d\n", result);
+
     print(pq);
+
      /*
     while (!p_queue_empty(pq))
     {
